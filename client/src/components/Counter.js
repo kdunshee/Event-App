246e82c49cad0fa77969e30fd0ccecdf38c038da
Kyle Dunshee <kpdunshee@gmail.com>
2020-06-30 09:25:32 -0600

    worked on button

--- client/src/components/Counter.js
+++ client/src/components/Counter.js
@@ -1,26 +1,25 @@
 import React, { useState } from "react";
+import {Button} from 'semantic-ui-react'
 
 const Counter = () => {
-  // Set the initial count state to zero, 0
+  
   const [count, setCount] = useState(0);
 
-  // Create handleIncrement event handler
+
   const handleIncrement = () => {
     setCount(prevCount => prevCount + 1);
   };
 
-  //Create handleDecrement event handler
-  const handleDecrement = () => {
-    setCount(prevCount => prevCount - 1);
-  };
+  
+  
   return (
     <div>
       <div>
-        <button onClick={handleDecrement}>-</button>
+    
         <h5>Count is {count}</h5>
-        <button onClick={handleIncrement}>+</button>
+        <Button onClick={handleIncrement}>Likes</Button>
       </div>
-      <button onClick={() => setCount(0)}>Reset</button>
+      <Button onClick={() => setCount(0)}>Reset</Button>
     </div>
   );
 }
